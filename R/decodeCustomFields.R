0f593c5bbc2015608b3b1eef26dc43bda6995075
Kim Gilbert <kimberly.gilbert2@unibe.ch>
2025-03-31 17:02:33 +0200

    updates to decodeCustomFields

--- R/decodeCustomFields.R
+++ R/decodeCustomFields.R
@@ -13,60 +13,85 @@
 #' all_tabs <- getPFData()
 #' project <- all_tabs$project
 #' customfields <- all_tabs$customfields
+#' customfieldvalue <- all_tabs$customfieldvalue
 #' project2 <- decodeCustomFields(project, customfields)
 #' @importFrom magrittr %>%
-decodeCustomFields <- function(project, customfields, trace = FALSE){
+ 
+
+
+decodeCustomFields <- function(dat, dat_ID_col, customfields, customfieldvalue, trace = FALSE){
   
   STOREKEY <- NAME <- PK_CUSTOMFIELD <- AKTIV <- AREA <- NULL
   
-  tags <- customfields %>% 
+  tags <- customfields %>%
     dplyr::mutate(
       storekey = as.character(STOREKEY),
       storekey = stringr::str_replace(storekey, "\\.", "\\\\."),
-      regex = stringr::str_c("(?<=<", storekey, ">)(.)+(?=</", storekey, ">)"),
-      # regex = stringr::str_c("(?<=<", storekey, ">).{1,}(?=</", storekey, ">)"),
       name = janitor::make_clean_names(NAME),
       name = stringr::str_c("cf_", name)
-    ) %>% 
-    dplyr::select(PK_CUSTOMFIELD, NAME, AKTIV, STOREKEY, storekey, AREA, regex, name) 
-    
-  tags <- tags %>% 
-    dplyr::filter(.data$STOREKEY %in% names(which(sapply(tags$STOREKEY, function(x) any(grepl(x, project$CUSTOMFIELDVALUES))))))
+    ) %>%
+    dplyr::select(PK_CUSTOMFIELD, NAME, AKTIV, STOREKEY, AREA, name)
   
-  out <- project
+  # just take the ID number I need
+  tags$storekey_trimmed <- sapply(strsplit(tags$STOREKEY, split = "\\."), function(x) x[2])
   
-  if(nrow(tags) > 0){
-    # res <- lapply(1:nrow(tags), function(x){
-    #   name <- tags$name[x]
-    #   regex <- tags$regex[x]
-    #   z <- data.frame(z = stringr::str_extract(project$CUSTOMFIELDVALUES, regex))
-    #   names(z) <- name
-    #   z
-    #   }) 
-    cf <- project$CUSTOMFIELDVALUES
-    for(x in 1:nrow(tags)){
-      if(trace) cat(paste(x, " "))
-      name <- tags$name[x]
-      regex <- tags$regex[x]
-      storekey <- tags$storekey[x]
-      out[, name] <- NA
-      tag_in <- which(!is.na(cf) & stringi::stri_detect_regex(cf, storekey))
-      cf[tag_in] <- stringr::str_replace_all(cf[tag_in],"\r\n","; ")
-      tag_in2 <- tag_in[grepl(regex, cf[tag_in], perl = TRUE)]
-      # out[tag_in, name] <- stringr::str_extract(out$CUSTOMFIELDVALUES[tag_in], regex)
-      out[tag_in2, name] <- regmatches(cf[tag_in2], 
-                                      regexpr(regex, cf[tag_in2], perl = TRUE))
-      out[tag_in[!tag_in %in% tag_in2], name] <- "REGEX extraction failed"
-      
-    }
-    
-    # out <- out %>% 
-    #   dplyr::bind_cols(res)
-  }
+  
+  # merge with customfieldvalue
+  new_cfs <- merge(x=customfieldvalue, y=tags, by.x="FK_CUSTOMFIELD", by.y="storekey_trimmed")
+  
+  # just get the custom field values and ignore the NAs
+  cf_values <- new_cfs[c("NUMBERVALUE", "DATEVALUE", "TEXTVALUE", "IDENTITYVALUE", "BOOLEANVALUE")]
+  
+  # can double check that there is only one value per row, but it is the case, and there is no empty row as also checked here
+  ##num_nas_if_four_empty <- 4*dim(cf_values)[1]
+  ##na_count <- sum(is.na(cf_values))
+  
+  cf_singlecolumn_value <- apply(cf_values, 1, function(x){ na.omit(x) })
+  new_cfs$cf_singlecolumn_value <- cf_singlecolumn_value
+  
+  # and remove the columns we don't need anymore
+  new_cfs <- new_cfs |>
+    select(FK_ITEM, name, cf_singlecolumn_value)
+  #    select(-c("FK_CUSTOMFIELD", "PK_CUSTOMFIELDVALUE", "NUMBERVALUE", "DATEVALUE", "TEXTVALUE", "IDENTITYVALUE", "BOOLEANVALUE"))
+  
+  
+  # reshape the single columns into their own columns for each cf
+  cfs_reshaped <- new_cfs %>%
+    pivot_wider(
+      names_from = name,    # New column names come from the 'name' column
+      values_from = cf_singlecolumn_value
+    )
+  
+  
+  # now merge with the dataframe of choice, e.g. tickets, projects, etc using FK_ITEM
+  #out <- merge(x=dat, y=cfs_reshaped, by.x=dat_ID_col, by.y ="FK_ITEM")
+  out <- dat %>%
+    left_join(cfs_reshaped, by = setNames("FK_ITEM", dat_ID_col))
   
   return(out)
-  
 }
+
+
+
+
+#' use the above decodeCustomFields function across all four instances of tables from pf
+#' list = all_tabs
+#' the direct backend download from projectfacts
+#' @param list
+#' 
+#' @export
+
+
+decodeAllCustomFields <- function(list){
+  cfs <- list$customfields
+  cfvs <- list$customfieldvalue
+  list$customer <- decodeCustomFields(dat=list$customer, dat_ID_col="PK_CUSTOMER", customfields=cfs, customfieldvalue=cfvs)
+  list$project <- decodeCustomFields(dat=list$project, dat_ID_col="PK_Project", customfields=cfs, customfieldvalue=cfvs)
+  list$ticket <- decodeCustomFields(dat=list$ticket, dat_ID_col="PK_TICKET", customfields=cfs, customfieldvalue=cfvs)
+  list$worker <- decodeCustomFields(dat=list$worker, dat_ID_col="PK_Worker", customfields=cfs, customfieldvalue=cfvs)
+  return(list)
+}
+
 
 # microbenchmark(
 #   decodeCustomFields(all_tabs$ticket, all_tabs$customfields),
